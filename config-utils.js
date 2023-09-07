9e0048d5a7d36d2ab0c393087f00287bf0325948
Eberuson <Eberuson@users.noreply.github.com>
2023-09-07 09:25:41 -0300

    Update config-utils.js

--- config-utils.js
+++ config-utils.js
@@ -12,7 +12,7 @@ new Promise(exportModule => {
 
         // Namespace/module
         const ModuleName = 'utils.config-utils';
-main
+barracks
         /** Script configurations */
         const _Configurations = {
             masterKey: 'TwFrameworkConfigs'
